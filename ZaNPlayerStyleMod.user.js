634f4dcd1d26a7f9f320c3416f6928bc08ed4ae0
あやびー <ayebee.ty@gmail.com>
2022-05-05 21:36:36 +0900

    Update ZaNPlayerStyleMod.user.js

--- ZaNPlayerStyleMod.user.js
+++ ZaNPlayerStyleMod.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Z-aN Player Style Mod
-// @namespace    http://ayebee.jp/
+// @namespace    https://github.com/AyeBee/ZaNPlayerStyleMod
 // @version      0.1
 // @description  動画配信サイトZ-aNの画面構成をちょっと良くするユーザースクリプトです。
 // @author       ayebee
